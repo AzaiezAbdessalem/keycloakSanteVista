eb76b02cfb86ca7717b480b5fafeb182f4a206e2
annabiInsy2s <mannabi@insy2s.fr>
2024-07-12 15:21:02 +0100

    update

---
+++ src/main/java/com/mskeycloak/model/User.java
@@ -0,0 +1,59 @@
+package com.mskeycloak.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * User entity.
+ * It is used to represent the users of the application.
+ * It is the account, used for the authentication and authorization.
+ * To not be confused with the user's information from the Member microservice.
+ */
+@Entity
+@Table(name = "users")
+@Builder
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+
+    @Id
+    @Column(name = "id")
+    private String id;
+
+
+    private String username;
+
+
+    private String email;
+
+
+    private String firstname;
+
+
+    private String lastname;
+
+
+    private String password;
+
+    private boolean enabled = true;
+
+    private boolean status = false;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Collection<Role> roles = new ArrayList<>();
+
+
+
+}
--- src/main/java/com/resturant/mskeycloak/error/FieldError.java
+++
@@ -1,9 +0,0 @@
-package com.resturant.mskeycloak.error;
-
-public record FieldError(
-        String entityName,
-        String fieldName,
-        String message,
-        String code
-) {
-}
